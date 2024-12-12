d29e478e64e608545ffac667bc7bb19324d63586
SimCab-CHU <70569769+SimCab-CHU@users.noreply.github.com>
2024-12-12 16:54:01 +0100

    Update generate_report_MobiCT.py

--- Generate_Interface/generate_report_MobiCT.py
+++ Generate_Interface/generate_report_MobiCT.py
@@ -1,8 +1,6 @@
 #######################################################
 #                  FUNCTIONS                          #
 #######################################################
-import re
-import json
 import pandas as pd
 import numpy as np
 import os
